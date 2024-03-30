80fe8b680a21baef72050b901d8f84b8a92c2d94
progmamun <mamun333089@gmail.com>
2024-03-30 19:04:28 +0600

    upper-screen

--- App.js
+++ App.js
@@ -1,13 +1,71 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, View } from "react-native";
+import {
+  Inter_400Regular,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+// import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  ActivityIndicator,
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Inter-Regular": Inter_400Regular,
+    "Inter-Bold": Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hey this is me. I'm React Native Developer!</Text>
-      <View></View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <ImageBackground
+      source={require("./assets/light-bg.png")}
+      style={{ flex: 1 }}
+    >
+      {/* Parent View  */}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-between",
+          marginTop: 32,
+          paddingHorizontal: 26,
+        }}
+      >
+        {/* upper portion of the screen  */}
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ flex: 1 }}>
+            <Text
+              style={{
+                fontFamily: "Inter-Regular",
+                fontSize: 12,
+                color: "#fff",
+              }}
+            >
+              “The science of operations, as derived from mathematics more
+              especially, is a science of itself, and has its own abstract truth
+              and value.”
+            </Text>
+            <Text
+              style={{
+                fontFamily: "Inter-Bold",
+                color: "#fff",
+                fontSize: 12,
+                marginTop: 8,
+              }}
+            >
+              - Ada Lovelace
+            </Text>
+          </View>
+          <Image source={require("./assets/refresh.png")} />
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # clock-app-react-native
+
+- `npx expo install @expo-google-fonts/inter expo-font` | [link](https://github.com/expo/google-fonts)
